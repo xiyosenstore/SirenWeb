ba550d73ee01b06d04331dddc617085707e16149
xiyosenstore <ziyosenstore@gmail.com>
2025-09-11 00:33:24 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ use crate::config::Config;
 use crate::proxy::*;
 
 use std::collections::HashMap;
-use base64::{engine::general_purpose::URL_SAFE, Engine as _};
-use serde_json::json;
 use uuid::Uuid;
 use worker::*;
 use once_cell::sync::Lazy;
@@ -16,24 +14,114 @@ use regex::Regex;
 static PROXYIP_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^.+-\d+$").unwrap());
 static PROXYKV_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"^([A-Z]{2})").unwrap());
 
+// Base URL for GitHub raw content
+static GITHUB_BASE_URL: &str = "https://raw.githubusercontent.com/AFRcloud/SirenWeb/refs/heads/master/web";
+
 #[event(fetch)]
 async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
     let uuid = env
         .var("UUID")
         .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
     let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
-    let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url, sub_page_url };
+    let main_page_url = env.var("MAIN_PAGE_URL").map(|x| x.to_string()).unwrap();
+    let sub_page_url = env.var("SUB_PAGE_URL").map(|x| x.to_string()).unwrap();
+    let link_page_url = env.var("LINK_PAGE_URL").map(|x| x.to_string()).unwrap();
+
+    let config = Config { 
+    uuid, 
+    proxy_addr: host,
+    proxy_port: 443, 
+    main_page_url, 
+    sub_page_url,
+    link_page_url,
+};
+
+
+    let url = req.url()?;
+    let path = url.path();
+
+    if path.starts_with("/css/") {
+        return handle_css_file(req).await;
+    } else if path.starts_with("/js/") {
+        return handle_js_file(req).await;
+    } else if path.starts_with("/images/") {
+        return handle_image_file(req).await;
+    }
 
     Router::with_data(config)
         .on_async("/", fe)
         .on_async("/sub", sub)
-        .on("/link", link)
+        .on_async("/link", link)
         .on_async("/:proxyip", tunnel)
         .on_async("/Benxx-Project/:proxyip", tunnel)
         .run(req, env)
         .await
+}
+
+async fn handle_css_file(req: Request) -> Result<Response> {
+    let url = req.url()?;
+    let filename = url.path().strip_prefix("/css/").unwrap_or("");
+    let css_url = format!("{}/css/{}", GITHUB_BASE_URL, filename);
+    let req = Fetch::Url(Url::parse(&css_url)?);
+    let mut res = req.send().await?;
+
+    if res.status_code() == 200 {
+        let css = res.text().await?;
+        let mut headers = Headers::new();
+        headers.set("Content-Type", "text/css")?;
+        headers.set("Cache-Control", "public, max-age=86400")?;
+        Ok(Response::ok(css)?.with_headers(headers))
+    } else {
+        Response::error("CSS file not found", 404)
+    }
+}
+
+async fn handle_js_file(req: Request) -> Result<Response> {
+    let url = req.url()?;
+    let filename = url.path().strip_prefix("/js/").unwrap_or("");
+    let js_url = format!("{}/js/{}", GITHUB_BASE_URL, filename);
+    let req = Fetch::Url(Url::parse(&js_url)?);
+    let mut res = req.send().await?;
+
+    if res.status_code() == 200 {
+        let js = res.text().await?;
+        let mut headers = Headers::new();
+        headers.set("Content-Type", "application/javascript")?;
+        headers.set("Cache-Control", "public, max-age=86400")?;
+        Ok(Response::ok(js)?.with_headers(headers))
+    } else {
+        Response::error("JavaScript file not found", 404)
+    }
+}
+
+async fn handle_image_file(req: Request) -> Result<Response> {
+    let url = req.url()?;
+    let filename = url.path().strip_prefix("/images/").unwrap_or("");
+    let image_url = format!("{}/images/{}", GITHUB_BASE_URL, filename);
+    let req = Fetch::Url(Url::parse(&image_url)?);
+    let mut res = req.send().await?;
+
+    if res.status_code() == 200 {
+        let image_data = res.bytes().await?;
+        let mut headers = Headers::new();
+
+        if filename.ends_with(".png") {
+            headers.set("Content-Type", "image/png")?;
+        } else if filename.ends_with(".jpg") || filename.ends_with(".jpeg") {
+            headers.set("Content-Type", "image/jpeg")?;
+        } else if filename.ends_with(".svg") {
+            headers.set("Content-Type", "image/svg+xml")?;
+        } else if filename.ends_with(".gif") {
+            headers.set("Content-Type", "image/gif")?;
+        } else {
+            headers.set("Content-Type", "application/octet-stream")?;
+        }
+
+        headers.set("Cache-Control", "public, max-age=86400")?;
+        Ok(Response::from_bytes(image_data)?.with_headers(headers))
+    } else {
+        Response::error("Image file not found", 404)
+    }
 }
 
 async fn get_response_from_url(url: String) -> Result<Response> {
@@ -50,91 +138,63 @@ async fn sub(_: Request, cx: RouteContext<Config>) -> Result<Response> {
     get_response_from_url(cx.data.sub_page_url).await
 }
 
+async fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    get_response_from_url(cx.data.link_page_url).await
+}
 
 async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
     let mut proxyip = cx.param("proxyip").unwrap().to_string();
-    if PROXYKV_PATTERN.is_match(&proxyip)  {
-        let kvid_list: Vec<String> = proxyip.split(",").map(|s|s.to_string()).collect();
+    if PROXYKV_PATTERN.is_match(&proxyip) {
+        let kvid_list: Vec<String> = proxyip.split(",").map(|s| s.to_string()).collect();
         let kv = cx.kv("SIREN")?;
         let mut proxy_kv_str = kv.get("proxy_kv").text().await?.unwrap_or("".to_string());
         let mut rand_buf = [0u8, 1];
         getrandom::getrandom(&mut rand_buf).expect("failed generating random number");
-        
-        if proxy_kv_str.len() == 0 {
+
+        if proxy_kv_str.is_empty() {
             console_log!("getting proxy kv from github...");
-            let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json")?);
+            let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/AFRcloud/ProxyList/refs/heads/main/kvProxyList.json")?);
             let mut res = req.send().await?;
             if res.status_code() == 200 {
                 proxy_kv_str = res.text().await?.to_string();
-                kv.put("proxy_kv", &proxy_kv_str)?.expiration_ttl(60 * 60 * 24).execute().await?; // 24 hours
+                kv.put("proxy_kv", &proxy_kv_str)?.expiration_ttl(60 * 60 * 24).execute().await?;
             } else {
                 return Err(Error::from(format!("error getting proxy kv: {}", res.status_code())));
             }
         }
-        
+
         let proxy_kv: HashMap<String, Vec<String>> = serde_json::from_str(&proxy_kv_str)?;
-        
-        // select random KV ID
+
         let kv_index = (rand_buf[0] as usize) % kvid_list.len();
         proxyip = kvid_list[kv_index].clone();
-        
-        // select random proxy ip
+
         let proxyip_index = (rand_buf[0] as usize) % proxy_kv[&proxyip].len();
         proxyip = proxy_kv[&proxyip][proxyip_index].clone().replace(":", "-");
     }
 
-    let upgrade = req.headers().get("Upgrade")?.unwrap_or_default();
-    if upgrade == "websocket".to_string() && PROXYIP_PATTERN.is_match(&proxyip) {
+    if PROXYIP_PATTERN.is_match(&proxyip) {
         if let Some((addr, port_str)) = proxyip.split_once('-') {
             if let Ok(port) = port_str.parse() {
                 cx.data.proxy_addr = addr.to_string();
                 cx.data.proxy_port = port;
             }
         }
-        
+    }
+
+    let upgrade = req.headers().get("Upgrade")?.unwrap_or("".to_string());
+    if upgrade == "websocket" {
         let WebSocketPair { server, client } = WebSocketPair::new()?;
         server.accept()?;
-    
+
         wasm_bindgen_futures::spawn_local(async move {
             let events = server.events().unwrap();
             if let Err(e) = ProxyStream::new(cx.data, &server, events).process().await {
-                console_error!("[tunnel]: {}", e);
+                console_log!("[tunnel]: {}", e);
             }
         });
-    
+
         Response::from_websocket(client)
     } else {
         Response::from_html("hi from wasm!")
     }
-
-}
-
-fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-    let host = cx.data.host.to_string();
-    let uuid = cx.data.uuid.to_string();
-
-    let vmess_link = {
-        let config = json!({
-            "ps": "siren vmess",
-            "v": "2",
-            "add": host,
-            "port": "80",
-            "id": uuid,
-            "aid": "0",
-            "scy": "zero",
-            "net": "ws",
-            "type": "none",
-            "host": host,
-            "path": "/KR",
-            "tls": "",
-            "sni": "",
-            "alpn": ""}
-        );
-        format!("vmess://{}", URL_SAFE.encode(config.to_string()))
-    };
-    let vless_link = format!("vless://{uuid}@{host}:443?encryption=none&type=ws&host={host}&path=%2FKR&security=tls&sni={host}#siren vless");
-    let trojan_link = format!("trojan://{uuid}@{host}:443?encryption=none&type=ws&host={host}&path=%2FKR&security=tls&sni={host}#siren trojan");
-    let ss_link = format!("ss://{}@{host}:443?plugin=v2ray-plugin%3Btls%3Bmux%3D0%3Bmode%3Dwebsocket%3Bpath%3D%2FKR%3Bhost%3D{host}#siren ss", URL_SAFE.encode(format!("none:{uuid}")));
-    
-    Response::from_body(ResponseBody::Body(format!("{vmess_link}\n{vless_link}\n{trojan_link}\n{ss_link}").into()))
 }
